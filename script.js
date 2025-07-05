a19cee102f4eb8d3a808d0cf349d04f6732d0f7e
e-leonel <86567945+e-leonel@users.noreply.github.com>
2025-07-05 16:16:41 -0600

    Archivo js agregado, diseño responsive para tablet y pc agregados.

---
+++ script.js
@@ -0,0 +1,17 @@
+/* Seleciona todos los elementos con el atributo href=# y para cada uno le agrega un evento: escucha por clics */
+document.querySelectorAll('a[href^="#"').forEach(ira => {
+
+    ira.addEventListener('click', function (e) {
+
+        /* e.preventDefault : previene el salto brusco a las secciones */
+        e.preventDefault();
+
+        /* destino : busca los ids que coinciden con los elementos destino y obtiene ese id. */
+        const destino = document.querySelector(this.getAttribute('href'));
+
+        if (destino) {
+            /* si el destino esta le agrega el efecto smooth al hacer clic */
+            destino.scrollIntoView({behavior: 'smooth'});
+        }
+    });
+});
